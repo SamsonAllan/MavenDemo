cb3f42176ffb1fa9cc71416decada8376f46faf7
Allan Samson <asamson21@gmail.com>
2024-02-10 08:42:07 -0800

    second commit

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Hello {
     public static void main(String[] args) {
 
+        // sample
         WebDriver driver = new ChromeDriver();
 
         Faker faker = new Faker();
